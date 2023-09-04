69027b5aaf339af30aae970fdf4812b776e536dc
handsomebrogrammer <handsomebrogrammer@gmail.com>
2023-09-04 01:47:32 +0100

    delete useless script for player

--- Assets/Scripts/VtPlayer.cs
+++ Assets/Scripts/VtPlayer.cs
@@ -4,20 +4,54 @@ using UnityEngine;
 
 public class VtPlayer : MonoBehaviour
 {
+    public float moveSpeed = 5;
+    public float dividerAnimationWalk = 1;
     public Transform m_FrontArmParentBone;
     public Transform m_BackArmParentBone;
-
     public Transform m_WeaponFront;
     public Transform m_WeaponBack;
 
+    private Animator m_Animator;
+    private SpriteRenderer m_SpriteRenderer;
+
     private void Start()
     {
-
+        m_Animator = GetComponent<Animator>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        Move();
         Aim();
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0.0f);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        // Define as condições para as animações
+        if (moveDirection.magnitude > 0)
+        {
+            m_Animator.SetBool("Moving", true);
+
+            if (moveSpeed > 5)
+            {
+                m_Animator.SetBool("Running", true);
+            }
+            else
+            {
+                m_Animator.SetBool("Running", false);
+            }
+        }
+        else
+        {
+            m_Animator.SetBool("Moving", false);
+        }
     }
 
     private void Aim()
